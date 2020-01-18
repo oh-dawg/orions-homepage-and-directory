3d8f3739087d21608864b716535283912e89743c
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-01-18 15:10:51 -0700

    added more content 1/18/2020

--- homepage.php
+++ homepage.php
@@ -8,9 +8,19 @@
 </head>
    <body>
    
-   <figure style="float:left; width: 55%;">
-      <img src="photoOfMe.jpg" width="175" height="140"><figcaption style="width: 170%;">Orion in repose</figcaption>
+   <div class="TitleBar">
+   
+	<p>Orion's HomePage </p>
+   
+    </div>
+   
+   <div class="main">
+   
+   <figure class="myImage">
+      <img src="photoOfMe.jpg" width="175" height="140"><figcaption class="theCaption">Orion in repose</figcaption>
       </figure>
+   
+   </div>
    
    <div class="Linkbloc">
    </div>
