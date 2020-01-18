336a1cecce769e2f3a9c0284d4f0a27988e091a6
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-01-17 18:46:53 -0700

    added pic and some content

--- homepage.php
+++ homepage.php
@@ -8,6 +8,10 @@
 </head>
    <body>
    
+   <figure style="float:left; width: 55%;">
+      <img src="photoOfMe.jpg" width="175" height="140"><figcaption style="width: 170%;">Orion in repose</figcaption>
+      </figure>
+   
    <div class="Linkbloc">
    </div>
    
