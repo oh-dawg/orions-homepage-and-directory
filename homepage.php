bba83185cfffa2f0af006f1827932a467610a4ec
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-01-18 15:33:10 -0700

    changes, and shut down files

--- homepage.php
+++ homepage.php
@@ -14,16 +14,30 @@
    
     </div>
    
+   <div class="leftsidenav">
+   
+   <a href="directory.html">directory</a>
+   
+   </div>
+   
    <div class="main">
    
    <figure class="myImage">
       <img src="photoOfMe.jpg" width="175" height="140"><figcaption class="theCaption">Orion in repose</figcaption>
       </figure>
    
+   <p> My name is Orion Christensen.</p>
+   <br>
+   <p> I live in Idaho, USA</p>
+   <br>
+   <p> I enjoy hiking, camping, and the great outdoors</p>   
+   <br>
+   
    </div>
    
-   <div class="Linkbloc">
-   </div>
+   <div class="rightsidenav">
+  
+    </div>
    
      </body>
 </html>
