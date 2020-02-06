bcf4986921f6b3f633c20e76f48dec3def08686b
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-02-05 17:26:19 -0700

    added some files

---
+++ viewScriptures.php
@@ -0,0 +1,35 @@
+<?php
+require "dbaccess.php";
+$db = get_db();
+//SELECT id, book, chapter, verse, content FROM scriptureBD;
+$query = 'SELECT id, book, chapter, verse, content FROM scriptureBD';
+$stmt = $db->prepare($query);
+$stmt->execute();
+$scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Scripture List</title>
+	</head>
+​
+<body>
+	<div>
+		<h1>Scripture Resources</h1>
+		<ul>
+			<?php
+			// Go through each result
+			foreach ($scriptures as $scripture)
+			{
+				$scripture_id 	= $scripture['id'];
+				$book 			= $scripture['book'];
+				$chapter 		= $scripture['chapter'];
+				$verse 			= $scripture['verse'];
+				$content 		= $scripture['content'];
+				echo "<li><h3><a href='scriptureLinks.php?scripture_id=$scripture_id'>$book $chapter:$verse</a><h3></li>";
+			}
+			?>
+		</ul>
+	</div>
+</body>
+</html>
