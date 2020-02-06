e4a82572688a8439f7597b7d211fa4f59f3e7229
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-02-05 18:19:25 -0700

    Update scriptureLinks.php

--- scriptureLinks.php
+++ scriptureLinks.php
@@ -2,7 +2,7 @@
 $scripture_id = htmlspecialchars($_GET['scripture_id']);
 require "dbConnect.php";
 $db = get_db();
-$stmt = $db->prepare('SELECT book, chapter, verse, content FROM scriptureBD WHERE id = :id');
+$stmt = $db->prepare('SELECT book, chapter, verse, content FROM scriptures WHERE id = :id');
 $stmt->bindValue(':id', $scripture_id, PDO::PARAM_INT);
 $stmt->execute();
 $scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
