7b6532ba725eb38fd2df4a056153ae49d9c81a4d
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-02-06 18:23:16 -0700

    added another php page

---
+++ viewBookRepository.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+   <head>
+   
+   <title>homepage</title>
+   <link rel="stylesheet" type="text/css" href="masterstylesheet.css">
+   
+   </head>
+   <body>
+   
+   <div class="TitleBar">
+   
+	<p>Orion's HomePage </p>
+   
+    </div>
+   
+   <div class="leftsidenav">
+   
+   <a href="directory.html">directory</a>
+   
+   </div>
+   
+   <div class="main" align="center">
+   
+   <h1> My name is Orion Christensen.</h1>
+   <br>
+   
+   </div>
+   
+    </body>
+</html>
