06398b441ef75a308ac34ff344e042df1a09e014
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-02-08 18:53:02 -0700

    Update viewWorldGenre.php

--- viewWorldGenre.php
+++ viewWorldGenre.php
@@ -8,6 +8,17 @@ if(isset($_get['book_title']))
 }
 $book_title = htmlspecialchars($_GET['book_title']);
 
+require('dbAccess.php');
+$db = getdb();
+
+$query = 'SELECT b.genre, w.worldgenre FROM worldgenretable w JOIN bookrepository b ON w.bookrepository_id = b.id WHERE b.id =:id';
+$stmt = $db->prepare($query);
+$stmt->bindValue(':id', $book_title, PDO::PARAM_STR);
+$stmt->execute();
+$books_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$book_code = $books_rows[0]['genre'];
+
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +28,17 @@ $book_title = htmlspecialchars($_GET['book_title']);
 	<title> World Genre</title>
    </head>
    <body>
-   <h1> World Genre's for title <?php echo $book_title ?></h1>
+   <h1> World Genres for title <?php echo $book_code; ?></h1>
+   <?php
+   
+   foreach($books_rows as $book_row)
+   {
+	   
+	   $content = $note_row['content'];
+	   echo "<p>$content</p>";
+	   
+   }
+   ?>
    
    </body>
 </html>
