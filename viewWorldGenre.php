55a3926e516eb802b365c00a642ba6d13527f813
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-02-12 14:48:55 -0700

    changed world genre

--- viewWorldGenre.php
+++ viewWorldGenre.php
@@ -12,9 +12,11 @@ $book_title = htmlspecialchars($_GET['book_title']);
 require('dbAccess.php');
 $db = getdb();
 
-$query = 'SELECT b.genre, w.worldgenre FROM worldgenretable w JOIN bookrepository b ON w.bookrepository_id = b.id WHERE b.id =:id';
+//$query = 'SELECT b.genre, w.worldgenre FROM worldgenretable w JOIN bookrepository b ON w.bookrepository_id = b.id WHERE b.id =:id';
+$query = 'SELECT genre, worldgenre FROM worldgenretable WHERE id =:id';
 $stmt = $db->prepare($query);
-$stmt->bindValue(':id', $book_title, PDO::PARAM_STR);
+//$stmt->bindValue(':id', $book_title, PDO::PARAM_STR);
+$stmt->bindValue(':id', $book_title, PDO::PARAM_INT);
 $stmt->execute();
 $books_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -29,17 +31,22 @@ $book_code = $books_rows[0]['genre'];
 	<title> World Genre</title>
    </head>
    <body>
+   
    <h1> World Genres for title <?php echo $book_code; ?></h1>
    <?php
+   
+   <ul>
    
    foreach($books_rows as $book_row)
    {
 	   
-	   $content = $note_row['content'];
+	   $content = $book_row['content'];
 	   echo "<p>$content</p>";
 	   
    }
    ?>
+   
+   </ul>
    
    </body>
 </html>
