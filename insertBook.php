308ec28e0a8fe15c1280ec8fc50e2e6eaef97663
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-02-13 17:07:41 -0700

    Update insertBook.php

--- insertBook.php
+++ insertBook.php
@@ -43,29 +43,3 @@ die(); // we always include a die after redirects. In this case, there would be 
        // to see.
 
 ?>
-
-<!DOCTYPE html>
-<html>
-   <head>
-   
-   <title>homepage</title>
-   <link rel="stylesheet" type="text/css" href="masterstylesheet.css">
-   
-   </head>
-   <body>
-   
-   <div class="TitleBar">
-   
-	<p>Orion's book repository </p>
-   
-    </div>
-   
-   <div class="leftsidenav">
-   
-   <a href="directory.html">directory</a>
-   
-   </div>
-   
-   <div class="main" align="center">
-   
-   </div>
