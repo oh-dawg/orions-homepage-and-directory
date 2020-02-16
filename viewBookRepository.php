70913df56cd9496a4325619442f6cd071c42c462
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-02-15 19:54:47 -0700

    Update viewBookRepository.php

--- viewBookRepository.php
+++ viewBookRepository.php
@@ -89,30 +89,5 @@ $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
    
    </div>
    
-   <div class = "edit_table" align = "center">
-   
-		<form id="bookEntryForm" method= "post" action="insertBook.php">
-	
-			<input type="text" id="txtGenre" name="txtGenre"></input>
-			<label for="txtGenre">Genre</label>
-			<br /><br />
-
-			<input type="text" id="txtTitle" name="txtTitle"></input>
-			<label for="txtTitle">Title</label>
-			<br /><br />
-
-			<input type="text" id="txtPurpose" name="txtPurpose"></input>
-			<label for="txtPurpose">Purpose</label>
-			<br /><br />
-
-			<label for="txtDescription">Description:</label><br />
-	<textarea id="txtDescription" name="txtDescription" rows="4" cols="50"></textarea>
-	<br /><br />
-	
-	<button id="submitRecord" name="validate" type="submit">submit</button>
-      
-      </form>
-	</div>
-   
     </body>
 </html>
