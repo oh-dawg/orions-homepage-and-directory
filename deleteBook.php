d23ce53008eb02d258b39fea0772be78d1394739
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-02-25 09:57:20 -0700

    Update deleteBook.php

--- deleteBook.php
+++ deleteBook.php
@@ -11,7 +11,7 @@ try
 	// Add the Scripture
 
 	// We do this by preparing the query with placeholder values
-	$query = 'DELETE FROM public.bookrepository WHERE id = :id';
+	$query = 'DELETE FROM public.bookrepository WHERE id = :id ON DELETE CASCADE';
 	$statement = $db->prepare($query);
 	
 	$statement->bindValue(':id', $id);
